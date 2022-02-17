e0e3a28b5e6ddb0612c7f58b71523bbc75a95a55
Ruslan Zhilov <ruslan.zhilov@mail.ru>
2022-02-17 03:00:30 +0300

    Almost done. One project (shop list) was not prepared because i had no enough time

---
+++ ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.implicitintents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import androidx.core.app.ShareCompat
+import android.provider.MediaStore
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mWebsiteEditText: EditText
+    private lateinit var mLocationEditText: EditText
+    private lateinit var mShareTextEditText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mWebsiteEditText = findViewById(R.id.website_edittext)
+        mLocationEditText = findViewById(R.id.location_edittext)
+        mShareTextEditText = findViewById(R.id.share_edittext)
+    }
+
+    fun openWebsite(view: View) {
+        val url: String = mWebsiteEditText.text.toString()
+        val webpage: Uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!")
+        }
+    }
+
+    fun openLocation(view: View) {
+        val loc = mLocationEditText.text.toString()
+        val addressUri = Uri.parse("geo:0,0?q=$loc")
+        val intent = Intent(Intent.ACTION_VIEW, addressUri)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!")
+        }
+    }
+
+    fun shareText(view: View) {
+        val txt = mShareTextEditText.text.toString()
+        val mimeType = "text/plain"
+        ShareCompat.IntentBuilder
+            .from(this)
+            .setType(mimeType)
+            .setChooserTitle(R.string.share_text_with)
+            .setText(txt)
+            .startChooser()
+    }
+
+    fun takepicture(view: View) {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivity(intent)
+    }
+}
